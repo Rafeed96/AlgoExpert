9b881d4f3c43961692184d73f16fc0486204e355
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-03 08:00:19 +0600

    Create Two_Sum.py

---
+++ Arrays/Easy/Two_Sum.py
@@ -0,0 +1,24 @@
+def twoNumberSum(array, targetSum):
+    
+    x = len(array)
+
+    ret = []
+
+    for i in range(0,x):
+        if i == (x-1):
+            break
+        for j in range((i+1),x):
+            if array[i] + array[j] == targetSum:
+                ret.append(array[i])
+                ret.append(array[j])
+    
+    print(ret)
+ 
+
+
+
+arr = [3, 5, -4, 8, 11, 1, -1, 6]
+target = 10
+
+
+twoNumberSum(arr, target)
