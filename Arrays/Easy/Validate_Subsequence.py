57a2764204ee2d5c4da3bbfdcbaa498fdfadc210
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-05 11:06:31 +0600

    Update Validate_Subsequence.py

--- Arrays/Easy/Validate_Subsequence.py
+++ Arrays/Easy/Validate_Subsequence.py
@@ -1,14 +1,20 @@
 def isValidSubsequence(array, sequence):
 
+    # done  16/24
     output = True    
+
+    for i in sequence:
+        if i not in array:
+            output = False
+
     
-    return
+    print(output) 
 
 
 
-array = [3, 5, -4, 8, 11, 1, -1, 6]
-sequence = [10]
+array = [5, 1, 22, 25, 6, -1, 8, 10],
+sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
 
 
-isValidSubsequence()
+isValidSubsequence(array, sequence)
 
