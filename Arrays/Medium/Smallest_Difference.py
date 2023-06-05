a6c75b1ff22803a0e84e48e639457bb4fcd1f5ad
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-05 10:50:49 +0600

    Update Smallest_Difference.py

--- Arrays/Medium/Smallest_Difference.py
+++ Arrays/Medium/Smallest_Difference.py
@@ -1,24 +1,33 @@
 def smallestDifference(arrayOne, arrayTwo):
-
     
     ret = []
     mininum = arrayOne[0] - arrayTwo[0]
-
+    tem = 0
     mininum = abs(mininum)
-    print(mininum)
+    #print(mininum)
+    ret = [arrayOne[0], arrayTwo[0]]
+
 
     for i in range(1,len(arrayOne)):        
 
-        for j in range(1, len(arrayTwo)):
-    #         # print(arrayTwo[j])
+        for j in range(0, len(arrayTwo)):
+            tem = (arrayOne[i] - arrayTwo[j])
+            tem = abs(tem)
 
+            if tem < mininum:
+                ret = []
+                mininum = tem
+                ret.append(arrayOne[i])
+                ret.append(arrayTwo[j])         
     
-    # return ret
-
-
-
+    return ret
 
 arrayOne = [-1, 5, 10, 20, 28, 3]
 arrayTwo = [26, 134, 135, 15, 17]
+
+
+
+
+
 
 smallestDifference(arrayOne, arrayTwo)
