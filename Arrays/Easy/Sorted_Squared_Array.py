5b3d4d8cd55e0346992e04c090ad657e301110e6
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-05 12:15:01 +0600

    Update Sorted_Squared_Array.py

--- Arrays/Easy/Sorted_Squared_Array.py
+++ Arrays/Easy/Sorted_Squared_Array.py
@@ -1,3 +1,17 @@
 def sortedSquaredArray(array):
     # Write your code here.
-    return []
+    sqrd = []
+    temp = 0
+    for i in array:
+        temp = i*i
+        sqrd.append(temp)
+
+    sqrd.sort()
+    print(sqrd)
+    return sqrd
+
+
+
+array = [-3, -2, -1]
+
+sortedSquaredArray(array)
