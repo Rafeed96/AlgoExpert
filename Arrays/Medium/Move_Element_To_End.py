331eb0a20e4936cdbb8a3d8a20feb7d3385e2b51
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-07 08:19:44 +0600

    Update Move_Element_To_End.py

--- Arrays/Medium/Move_Element_To_End.py
+++ Arrays/Medium/Move_Element_To_End.py
@@ -1,3 +1,15 @@
 def moveElementToEnd(array, toMove):
     # Write your code here.
-    pass
+
+    n = len(array)
+    end = []
+    for i in range(0,n):        
+        if array[i] == toMove:            
+            array.remove(array[i])
+            array.append(toMove)
+                        
+    return array
+    
+arr = [2,1,2,2,2,3,4,2]
+num = 2
+moveElementToEnd(arr, num)
