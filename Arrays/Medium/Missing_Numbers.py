f6089731996cff268d79047da42be42ea53805a7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-08 04:41:02 +0600

    Update Missing_Numbers.py

--- Arrays/Medium/Missing_Numbers.py
+++ Arrays/Medium/Missing_Numbers.py
@@ -1,3 +1,22 @@
 def missingNumbers(nums):
     # Write your code here.
-    return []
+    arr = []
+
+    n = len(nums)
+    n = n+2
+    nums.sort()
+
+    for i in range(1,n+1):
+        if i in nums:
+            continue
+        else:
+            nums.append(i)
+            arr.append(i)
+
+    return arr
+
+
+
+
+nums = [1,4,3]
+missingNumbers(nums)
