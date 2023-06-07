ab2415c390e61cbb1c1d43bfffe72b1b9db52c74
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-08 04:30:46 +0600

    Update Array_Of_Products.py

--- Arrays/Medium/Array_Of_Products.py
+++ Arrays/Medium/Array_Of_Products.py
@@ -1,3 +1,24 @@
 def arrayOfProducts(array):
     # Write your code here.
-    pass
+
+    arr = []
+    temp = 1
+    n = len(array)
+
+    for i in range(0, n):
+        temp = 1
+        for j in range(0, n):
+            if j == i:
+                continue
+            else:
+                temp = temp * array[j]
+
+        arr.append(temp)
+
+    return arr
+
+
+
+array = [5, 1, 4, 2]
+
+arrayOfProducts(array)
