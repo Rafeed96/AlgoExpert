1993cb87cd7497959132d97fa5f2ebead0fc0fb4
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-08 06:42:50 +0600

    Update Majority_Element.py

--- Arrays/Medium/Majority_Element.py
+++ Arrays/Medium/Majority_Element.py
@@ -1,3 +1,30 @@
 def majorityElement(array):
     # Write your code here.
-    return -1
+
+    arr = []
+    n = len(array)
+    array.sort()
+    freq = []
+    count = 0
+    max_freq = 0
+    for i in range(0,n-1):
+        if array[i+1] == array[i]:
+            count = count +1
+        elif array[i+1] != array[i]:
+            arr.append(array[i])
+            freq.append(count)
+            count = 0
+
+            
+
+
+
+    print(arr)
+    print(freq)
+
+    return arr
+
+
+array = [1,2,3,2,2,1,2]
+
+majorityElement(array)
