865bb25f64bd40b78296df2b9d9b8c5ec3bcea9f
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-09 13:19:14 +0600

    Update Sweet_And_Savory.py

--- Arrays/Medium/Sweet_And_Savory.py
+++ Arrays/Medium/Sweet_And_Savory.py
@@ -1,3 +1,30 @@
 def sweetAndSavory(dishes, target):
     # Write your code here.
+    
+    dishes.sort()
+    print(dishes)
+    print(target)
+
+    sweet = []
+    savory = []
+
+    for i in dishes:
+        if i < 0:
+            sweet.append(i)
+        else:
+            savory.append(i)
+
+    print("Sweet:",sweet)
+    print("Savory:",savory)
+
+    
+
     return []
+
+
+
+
+dishes = [2, 5, -4, -7, 12, 100, -25]
+target = -5
+
+sweetAndSavory(dishes, target)
