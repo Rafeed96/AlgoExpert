3394f74e09dad60b31cc694f18ad711757a4f0bf
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-09 13:35:25 +0600

    Update Sweet_And_Savory.py

--- Arrays/Medium/Sweet_And_Savory.py
+++ Arrays/Medium/Sweet_And_Savory.py
@@ -2,8 +2,6 @@ def sweetAndSavory(dishes, target):
     # Write your code here.
     
     dishes.sort()
-    print(dishes)
-    print(target)
 
     sweet = []
     savory = []
@@ -14,15 +12,25 @@ def sweetAndSavory(dishes, target):
         else:
             savory.append(i)
 
-    print("Sweet:",sweet)
-    print("Savory:",savory)
+    dist = min(sweet) + max(savory) 
 
-    
+    far = 0
+
+    final = []
+    temp = 0
+    for i in sweet:
+        print("For : ", i , " With Target : " , target)
+        for j in savory:
+            temp = i + j
+            print("Sweet + Savory : ", temp)
+            if target > 0:
+                far = target - temp
+            else:
+                far = target + temp
+            
+
 
     return []
-
-
-
 
 dishes = [2, 5, -4, -7, 12, 100, -25]
 target = -5
