69d4afb57f40a3b31743ba650d08502d6a2bcddc
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-14 04:58:18 +0600

    Update Montonic_Array.py

--- Arrays/Medium/Montonic_Array.py
+++ Arrays/Medium/Montonic_Array.py
@@ -1,34 +1,38 @@
 def isMonotonic(array):
     # Write your code here.
-    
-    neg = True
-    pos = True
+
     p_count = 0
     n_count = 0
     n = len(array)
 
-    posA = array
-    negA = array[::-1]
-    
+    posA = []
+    negA = []
+    for i in array:
+        posA.append(i)
+
+    posA.sort()
+    negA = posA[::-1]
+
+
+
     for i in range(0, n):
         if array[i] != posA[i]:
             p_count = p_count + 1
         
         if array[i] != negA[i]:
             n_count = n_count + 1
-        
-    
-    if n_count or p_count == 0:
-        out = True
-    else:
-        out = False
 
+
+
+    out = True  
+    if n_count or p_count >= 0:
+        out = False
 
     return out
 
 
 
-array= [-1, -5, -10, -1100, -1101, -1102, -9001]
+array= [1, 1, 1, 2, 3, 4, 1]
 
 isMonotonic(array)
 
@@ -63,4 +67,47 @@ def isMonotonic(array):
         out = False
 
     return out
+'''
+
+
+'''
+def isMonotonic(array):
+    # Write your code here.
+
+    p_count = 0
+    n_count = 0
+    n = len(array)
+
+    posA = []
+    negA = []
+    for i in array:
+        posA.append(i)
+
+    posA.sort()
+    negA = posA[::-1]
+
+
+    print(posA)
+    print(negA)
+    print(array)
+
+    for i in range(0, n):
+        if array[i] != posA[i]:
+            p_count = p_count + 1
+        
+        if array[i] != negA[i]:
+            n_count = n_count + 1
+
+        print("-----------", array[i], "Pos: ",posA[i], " Neg: ",negA[i])
+
+
+    out = True  
+    print(p_count," " ,n_count)
+    if n_count or p_count >= 0:
+        out = False
+
+
+    print(out)
+    return out
+
 '''
