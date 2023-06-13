632b898890002331fdf8042a62874b57408a5d62
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-14 04:23:51 +0600

    Update Sweet_And_Savory.py

--- Arrays/Medium/Sweet_And_Savory.py
+++ Arrays/Medium/Sweet_And_Savory.py
@@ -18,18 +18,30 @@ def sweetAndSavory(dishes, target):
 
     final = []
     temp = 0
+
     for i in sweet:
         print("For : ", i , " With Target : " , target)
         for j in savory:
             temp = i + j
-            print("Sweet + Savory : ", temp)
+            print("Sweet + Savory : ", (j) ," ", temp)
+            if(temp == target):
+                final.append(i)
+                final.append(j)
+                break
+
             if target > 0:
                 far = target - temp
+                if far <= dist :
+                    dist = far
+
             else:
                 far = target + temp
+                if far >= dist :
+                    dist = far
+
             
 
-
+    print(final)
     return []
 
 dishes = [2, 5, -4, -7, 12, 100, -25]
