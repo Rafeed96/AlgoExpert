11804ffcdf9a20515fcd29c956e29695efbaa569
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-28 22:29:05 +0600

    Update First_Non_Repeating_Character.py

--- String/Easy/First_Non_Repeating_Character.py
+++ String/Easy/First_Non_Repeating_Character.py
@@ -1,16 +1,20 @@
 def firstNonRepeatingCharacter(string):
 
     ind = 0
+    fl = False
     n = len(string)
     count = 0
-    
+    l = []
     for i in range(0, n):
         count = 1
-    
-        for j in range(i, n):
-            if string[i] == string[j]:
-                count = count + 1
-                continue
+        if string[i] not in l:
+            l.append(string[i])
+            for j in range(i, n):
+                if string[i] == string[j]:
+                    count = count + 1
+                    
+        else:
+            continue
 
 
     return ind
