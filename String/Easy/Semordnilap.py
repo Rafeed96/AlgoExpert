703e2750be33523af86e4b2efa44ef9a7927b391
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-28 23:31:07 +0600

    Update Semordnilap.py

---
+++ String/Easy/Semordnilap.py
@@ -0,0 +1,16 @@
+def semordnilap(words):
+
+    semor = []
+    n = len(words)
+
+    for i in range(0, n):
+        l = words[i]
+
+        print(i, " ", l)
+
+    return semor
+
+
+
+words = ["diaper", "abc", "test", "cba", "repaid",] 
+semordnilap(words)
