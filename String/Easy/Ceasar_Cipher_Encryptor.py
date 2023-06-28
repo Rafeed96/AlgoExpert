6fd40d8e9b12b44197364c0d94c3e25dbe272ad9
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-28 09:50:42 +0600

    Update Ceasar_Cipher_Encryptor.py

---
+++ String/Easy/Ceasar_Cipher_Encryptor.py
@@ -0,0 +1,16 @@
+def caesarCipherEncryptor(string, key):
+    
+    for i in range(0, len(string)):
+        n = ord(string[i])
+        n = n + 1
+        print(n)
+        
+
+    pass
+
+
+string = "xyz"
+key = 2
+
+
+caesarCipherEncryptor(string, key)
