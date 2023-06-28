8782320380b7ab3dd5cec0276ac8d5bf5d86b51e
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-28 21:35:09 +0600

    Update Run_Length_Encoding.py

--- String/Easy/Run_Length_Encoding.py
+++ String/Easy/Run_Length_Encoding.py
@@ -4,6 +4,7 @@ def runLengthEncoding(string):
     n = len(string)
     count = 0
     for i in range(0, n-1):
+        print(string[i])
         count = count + 1
 
         if count > 8:
@@ -16,7 +17,18 @@ def runLengthEncoding(string):
             st = st + l + string[i]
             count = 0
 
-    print(st)
+        if i == n-2:
+            if string[i] == string[i+1]:
+                count = count + 1
+                l = str(count)
+                st = st + l + string[i]
+                count = 0
+            else:
+                l = str(count)
+                st = st + l + string[i]
+                count = 1
+                l = str(count)
+                st = st + l + string[i+1]
 
     return st
 
