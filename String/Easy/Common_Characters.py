58920dbc76a47c472f1180392f989f6603f2aa42
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-29 22:48:42 +0600

    Update Common_Characters.py

--- String/Easy/Common_Characters.py
+++ String/Easy/Common_Characters.py
@@ -1,3 +1,21 @@
 def commonCharacters(strings):
-    # Write your code here.
-    return []
+    
+    k = strings[0]
+    final = []
+    n = len(strings)
+    for i in k:
+        flag = True
+       
+        for j in range(1,n):
+            l = strings[j]
+            if i not in l:
+                flag = False
+
+        if flag == True:
+            final.append(i)
+
+
+    return final
+
+strings = ["abc", "bcd", "cbad"]
+commonCharacters(strings)
