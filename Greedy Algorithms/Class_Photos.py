70a27e656161decf14659f918dbb3f7948f6f68a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-29 23:27:50 +0600

    Update Class_Photos.py

--- Greedy Algorithms/Class_Photos.py
+++ Greedy Algorithms/Class_Photos.py
@@ -1,3 +1,23 @@
 def classPhotos(redShirtHeights, blueShirtHeights):
-    # Write your code here.
-    return False
+    
+    out = True
+    n = len(redShirtHeights)
+
+    redTaller = True
+    blueTaller = True
+    for i in range(0,n):
+        if redShirtHeights[i] < blueShirtHeights[i]:
+            redTaller = False
+
+        if redShirtHeights[i] > blueShirtHeights[i]:
+            blueTaller = False
+
+    return out
+
+
+
+
+redShirtHeights = [5, 8, 1, 3, 4]
+blueShirtHeights = [6, 9, 2, 4, 5]
+
+classPhotos(redShirtHeights, blueShirtHeights)
