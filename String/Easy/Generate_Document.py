e111c8e35099bcd7f1c81196493158fe3ca335a4
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-29 21:30:32 +0600

    Update Generate_Document.py

--- String/Easy/Generate_Document.py
+++ String/Easy/Generate_Document.py
@@ -1,9 +1,29 @@
 def generateDocument(characters, document):
     
     out = True
+    cDict = {}
+    n = len(characters)
+    for i in range(0,n):
+        count = 1
+        if characters[i] not in cDict:
+            cDict[characters[i]] = count
+        else:
+            x = cDict.get(characters[i])
+            x = x + count
+            cDict.update({characters[i]: x})
 
+    dDict = {}
+    n = len(document)
+    for i in range(0,n):
+        count = 1
+        if document[i] not in dDict:
+            dDict[document[i]] = count
+        else:
+            x = dDict.get(document[i])
+            x = x + count
+            dDict.update({document[i]: x})
 
-
+    print(dDict)
     return out
 
 
