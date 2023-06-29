07cc78315aa623ec102ddab0671744f812bfaacb
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-29 23:48:12 +0600

    Update Spiral_Traverse.py

--- Arrays/Medium/Spiral_Traverse.py
+++ Arrays/Medium/Spiral_Traverse.py
@@ -1,3 +1,26 @@
 def spiralTraverse(array):
-    # Write your code here.
-    pass
+    
+    final = []
+
+    row = 0 
+    col = 0
+
+    n = len(array)
+    m = n
+    
+    for i in range(0, n):
+        for j in range(0, m):
+            print(array[i][j])
+        
+
+    return final
+
+
+array = [
+    [1, 2, 3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10, 9, 8, 7]
+  ]
+
+spiralTraverse(array)
