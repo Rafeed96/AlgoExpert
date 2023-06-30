1639953c43d2d09223892d190f23c790d236d10a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-30 22:34:07 +0600

    Update Minimum_Characters_For_Words.py

--- String/Medium/Minimum_Characters_For_Words.py
+++ String/Medium/Minimum_Characters_For_Words.py
@@ -8,9 +8,10 @@ def minimumCharactersForWords(words):
         charDict = {}
         for j in range(0,n):
             count = 1
+            temp.append(i[j])
             for k in range(j+1,n):
-                if i[j] == i[k]:
-                    count = count +1
+                if i[j] == i[k] and i[j] in temp:
+                    count = count + 1
             
             charDict[i[j]] = count
         
