606e36755bc1c1808296103d45ee569fa5a07944
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-30 20:51:56 +0600

    Update Reverse_Words_In_String.py

--- String/Medium/Reverse_Words_In_String.py
+++ String/Medium/Reverse_Words_In_String.py
@@ -1,10 +1,17 @@
 def reverseWordsInString(string): 
     
-    st = ""    
+    st = ""   
+    rev = "" 
+    final = []
     n = len(string)
     for i in range(0,n):
-        print(string[i])
-    
+        if string[i] == " " or i == n-1:
+            final.append(st)
+            st = ""
+        else:
+            st = st + string[i]
+
+    print(final)
     return st
 
 
