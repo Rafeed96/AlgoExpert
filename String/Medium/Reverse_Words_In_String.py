403bbebe380a95a1edd28e9d335a9a8db465ee00
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-30 20:44:15 +0600

    Update Reverse_Words_In_String.py

---
+++ String/Medium/Reverse_Words_In_String.py
@@ -0,0 +1,14 @@
+def reverseWordsInString(string): 
+    
+    st = ""    
+    n = len(string)
+    for i in range(0,n):
+        print(string[i])
+    
+    return st
+
+
+
+
+string = "AlgoExpert is the best!"
+reverseWordsInString(string)
