3fc8d03adf836e71d6b7f2f4f589b837181c8104
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 21:17:22 +0600

    Update First_Duplicate_Value.py

--- Arrays/Medium/First_Duplicate_Value.py
+++ Arrays/Medium/First_Duplicate_Value.py
@@ -1,32 +1,28 @@
 def firstDuplicateValue(array):
     # Write your code here.
 
-    # need to count distance in between
-
     n = len(array)
     dist = n
     temp = n
     final = 0
+    ind = 0
     out = False
     for i in range(0, n):
         val = array[i]
         for j in range(i+1, n):
             if val == array[j]:
-                temp = j-i
+                temp = j
                 if temp < dist:
                     dist = temp
                     final = val
                 continue
 
- 
-
-    print(final," ", dist, " ", temp)
     return final
 
 
 
 
 
-array = [13, 4, 10, 10, 8, 13, 13, 7, 11, 6, 3, 2, 11]
+array = [3, 1, 3, 1, 1, 4, 4]
 
 firstDuplicateValue(array)
