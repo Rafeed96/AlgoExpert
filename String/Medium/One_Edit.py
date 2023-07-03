edccd4b57d9c22a60af1243e84e63e0b3ba6f0d4
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 23:47:26 +0600

    Update One_Edit.py

---
+++ String/Medium/One_Edit.py
@@ -0,0 +1,13 @@
+def oneEdit(stringOne, stringTwo):
+    # Write your code here.
+    out = False
+
+    if stringOne == stringTwo :
+        out = True
+        return out
+
+
+stringOne = "aaa"
+stringTwo = "aaa"
+
+oneEdit(stringOne, stringTwo)
