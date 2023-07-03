133c4bd1c918c46599537c739b06af96905cd972
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 23:17:45 +0600

    Update Product_Sum.py

--- Recursion/Easy/Product_Sum.py
+++ Recursion/Easy/Product_Sum.py
@@ -2,6 +2,24 @@
 # is a list or an integer.
 def productSum(array):
     # Write your code here.
-    pass
+
+    sum = 0
+    for i in array:
+        if type(i) == int:
+            sum = sum + array[i]
+        else:
+            out = productSum(i)
+            sum = sum + out
+        
+    
 
 
+    return sum
+
+
+
+
+array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+
+
+productSum(array)
