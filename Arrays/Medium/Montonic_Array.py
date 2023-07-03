ecc02d61eacd572523578123e4de56943ed3b533
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 21:25:47 +0600

    Update Montonic_Array.py

--- Arrays/Medium/Montonic_Array.py
+++ Arrays/Medium/Montonic_Array.py
@@ -5,13 +5,12 @@ def isMonotonic(array):
     n_count = 0
     n = len(array)
 
-    posA = []
-    negA = []
-    for i in array:
-        posA.append(i)
+    for i in range(0,n-1):
 
-    posA.sort()
-    negA = posA[::-1]
+        if array[i] > array[i+1]:
+            
+
+
 
 
 
@@ -28,51 +27,18 @@ def isMonotonic(array):
     if n_count or p_count >= 0:
         out = False
 
+    print(out)
     return out
 
 
 
-array= [1, 1, 1, 2, 3, 4, 1]
+array= [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
 
 isMonotonic(array)
 
 '''
 def isMonotonic(array):
     # Write your code here.
-    
-    neg = True
-    pos = True
-    p_count = 0
-    n_count = 0
-    n = len(array)
-
-    for i in range(1, n):
-
-        if array[i] >= array[i-1]:
-            pos = True
-        else:
-            pos = False
-            p_count = p_count + 1
-
-        
-        if array[i] <= array[i-1]:
-            neg = True
-        else:
-            neg = False
-            n_count = n_count + 1
-    
-    if n_count or p_count == 0:
-        out = True
-    else:
-        out = False
-
-    return out
-'''
-
-
-'''
-def isMonotonic(array):
-    # Write your code here.
 
     p_count = 0
     n_count = 0
@@ -87,9 +53,6 @@ def isMonotonic(array):
     negA = posA[::-1]
 
 
-    print(posA)
-    print(negA)
-    print(array)
 
     for i in range(0, n):
         if array[i] != posA[i]:
@@ -98,16 +61,14 @@ def isMonotonic(array):
         if array[i] != negA[i]:
             n_count = n_count + 1
 
-        print("-----------", array[i], "Pos: ",posA[i], " Neg: ",negA[i])
 
 
     out = True  
-    print(p_count," " ,n_count)
     if n_count or p_count >= 0:
         out = False
 
-
     print(out)
     return out
+
 
 '''
