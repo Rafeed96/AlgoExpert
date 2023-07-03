695bd0a53f45a1498fbc058f85018b32a814cef8
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 23:31:31 +0600

    Update Staircase_Traversal.py

--- Recursion/Medium/Staircase_Traversal.py
+++ Recursion/Medium/Staircase_Traversal.py
@@ -2,4 +2,12 @@ def staircaseTraversal(height, maxSteps):
     # Write your code here.
     steps = 0
 
+    
+
     return steps
+
+
+
+height = 4
+maxSteps = 2 
+staircaseTraversal(height, maxSteps)
