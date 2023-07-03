ba57c1e8cef067ed13ba342995fea9b122a67449
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-03 21:41:04 +0600

    Update Montonic_Array.py

--- Arrays/Medium/Montonic_Array.py
+++ Arrays/Medium/Montonic_Array.py
@@ -13,7 +13,6 @@ def isMonotonic(array):
         if array[i] < array[i+1]:
             p_count = False
 
-
     out = True  
     if n_count == True or p_count == True:
         out = True
@@ -22,45 +21,6 @@ def isMonotonic(array):
 
     return out
 
-
-
 array= [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
 
 isMonotonic(array)
-
-'''
-def isMonotonic(array):
-    # Write your code here.
-
-    p_count = 0
-    n_count = 0
-    n = len(array)
-
-    posA = []
-    negA = []
-    for i in array:
-        posA.append(i)
-
-    posA.sort()
-    negA = posA[::-1]
-
-
-
-    for i in range(0, n):
-        if array[i] != posA[i]:
-            p_count = p_count + 1
-        
-        if array[i] != negA[i]:
-            n_count = n_count + 1
-
-
-
-    out = True  
-    if n_count or p_count >= 0:
-        out = False
-
-    print(out)
-    return out
-
-
-'''
