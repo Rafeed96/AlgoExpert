d765fb848bf61e17eb2e1887d9118ca75e14e9c1
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-04 20:58:19 +0600

    Update Tandem_Bicycle.py

--- Greedy Algorithms/Easy/Tandem_Bicycle.py
+++ Greedy Algorithms/Easy/Tandem_Bicycle.py
@@ -1,3 +1,31 @@
 def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
     # Write your code here.
-    return 0
+    
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+
+    blueShirtSpeeds = blueShirtSpeeds[::-1]
+
+    tots = 0
+
+    n = len(redShirtSpeeds)
+
+    for i in range(0,n):
+
+        if redShirtSpeeds[i] > blueShirtSpeeds[i]:
+            tots = tots + redShirtSpeeds[i]
+        elif blueShirtSpeeds[i] > redShirtSpeeds[i]:
+            tots = tots + blueShirtSpeeds[i]
+        else:
+            tots = tots + blueShirtSpeeds[i]
+
+
+    return tots
+
+
+
+redShirtSpeeds =  [5, 5, 3, 9, 2]
+blueShirtSpeeds = [3, 6, 7, 2, 1]
+fastest = True
+
+tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest)
