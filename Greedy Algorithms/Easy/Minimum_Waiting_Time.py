537f6d0e894f3daf3e746e0ff5e0fa376e75e452
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-04 20:08:52 +0600

    Update Minimum_Waiting_Time.py

--- Greedy Algorithms/Easy/Minimum_Waiting_Time.py
+++ Greedy Algorithms/Easy/Minimum_Waiting_Time.py
@@ -1,11 +1,27 @@
 def minimumWaitingTime(queries):
     # Write your code here.
     time = 0
+    out = []
+    n = len(queries)
+    queries.sort()
+    print(queries)
 
-    for i in queries:
-        time = time + i
+    for i in range(0,n):
+        if i == 0:
+            out.append(0)
+            continue
 
-    time = time + 1
+        else:
+            sum = 0
+            for j in range(0,i):
+                sum = sum + queries[j]
+
+            out.append(sum)
+        
+    
+
+        
+
 
     return time 
 
